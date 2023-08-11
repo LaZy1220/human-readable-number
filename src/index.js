d1bef37af4f60e39bc90c31dc1b38a2d099263c4
LaZy1220 <4elove4iska@gmail.com>
2023-08-11 12:00:08 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number === 0) {
+        return "zero";
+    }
+
+    if (number < 20) {
+        return ones[number];
+    }
+
+    if (number < 100) {
+        const ten = Math.floor(number / 10);
+        const one = number % 10;
+        return tens[ten] + (one !== 0 ? " " + ones[one] : "");
+    }
+
+    if (number < 1000) {
+        const hundred = Math.floor(number / 100);
+        const remainder = number % 100;
+        return (
+            ones[hundred] +
+            " hundred" +
+            (remainder !== 0 ? " " + toReadable(remainder) : "")
+        );
+    }
+
+    return number.toString();
+};
